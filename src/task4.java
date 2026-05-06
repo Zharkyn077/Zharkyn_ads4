a898efadbab2f6ae52d26c1bb0f8998eb5628e41
Zharkyn077 <zhenisuly_zh@icloud.com>
2026-05-06 21:05:48 +0500

    task4.java

--- src/task4.java
+++ src/task4.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+//Shortest path from Edinburgh to Dundee
 
-public class task4 {
-}
+//Edges:
+//Edinburgh–Perth = 100
+//Perth–Dundee = 60
+//Edinburgh–Stirling = 50
+//Stirling–Perth = 40
+//Glasgow–Stirling = 50
+//Glasgow–Edinburgh = 70
+
+//Possible paths:
+ //       1) Edinburgh → Perth → Dundee = 160
+   //     2) Edinburgh → Stirling → Perth → Dundee = 150
+
+//Shortest path:
+//Edinburgh → Stirling → Perth → Dundee
+
+  //      Distance = 150
