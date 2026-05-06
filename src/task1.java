c38e5e3780cc781da2618f807fbab61341fb359c
Zharkyn077 <zhenisuly_zh@icloud.com>
2026-05-06 21:03:38 +0500

    task1.java

--- src/task1.java
+++ src/task1.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+//DFS traversal starting from A
 
-public class task1 {
-}
+//Graph:
+//A: C B D
+//B: A C E G
+//C: A B D
+//D: C A
+//E: G F B
+//F: G E
+//G: F B
+
+//Step-by-step:
+//A → C → B → E → G → F → D
+
+//Final DFS order:
+//A C B E G F D
