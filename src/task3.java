3ff8b6829f94e34a0c067a8acd06568d8f4dfff9
Zharkyn077 <zhenisuly_zh@icloud.com>
2026-05-06 21:05:20 +0500

    task3.java

--- src/task3.java
+++ src/task3.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class task3 {
+
+    static Map<String, List<String>> graph = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        graph.put("A", Arrays.asList("C", "B", "D"));
+        graph.put("B", Arrays.asList("A", "C", "E", "G"));
+        graph.put("C", Arrays.asList("A", "B", "D"));
+        graph.put("D", Arrays.asList("C", "A"));
+        graph.put("E", Arrays.asList("G", "F", "B"));
+        graph.put("F", Arrays.asList("G", "E"));
+        graph.put("G", Arrays.asList("F", "B"));
+
+        System.out.println("DFS:");
+        dfs("A", new HashSet<>());
+
+        System.out.println("\nBFS:");
+        bfs("A");
+    }
+
+    static void dfs(String node, Set<String> visited) {
+        if (visited.contains(node)) return;
+
+        visited.add(node);
+        System.out.print(node + " ");
+
+        for (String neighbor : graph.get(node)) {
+            dfs(neighbor, visited);
+        }
+    }
+
+    static void bfs(String start) {
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            String node = queue.poll();
+            System.out.print(node + " ");
+
+            for (String neighbor : graph.get(node)) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 }
